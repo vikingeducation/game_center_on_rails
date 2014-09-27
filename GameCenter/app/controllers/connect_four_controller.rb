ebbd31eab2e15d26c57e927659f003a178b35cc9
Tom McLaughlin <tim5046@gmail.com>
2014-09-27 19:02:10 -0400

    board movement complete

--- GameCenter/app/controllers/connect_four_controller.rb
+++ GameCenter/app/controllers/connect_four_controller.rb
@@ -1,15 +1,26 @@
 class ConnectFourController < ApplicationController
   def index 
-    @board = Connectfour.new
-    @board = @board.board.board
+
+    @game = Connectfour.new(session['current_board'])
+    @board = @game.board.board
+
+    session['current_board'] = @board
+    session['game'] = @game
   end 
 
   def select_create
+        session['current_board'] = nil
+    session['game'] = nil
     redirect_to connect_four_index_path
   end
 
   def create
+    #update current board
+    @game = session['game']
+    @update = @game.check_move(params[:c4choice])# unless params[:c4choice].nil?
+    session['current_board'] = @update.board
     redirect_to connect_four_index_path
   end
+
 
 end
--- GameCenter/app/models/connectfour.rb
+++ GameCenter/app/models/connectfour.rb
@@ -4,20 +4,28 @@ class Connectfour
     if board.nil?
       @board = Board.new
     else
-      @board = Board.new(@board)
+      @board = Board.new(board)
     end
   end
+
+
+
+  def check_move(column)
+    if valid_move?(column)
+      add_piece(column)
+    end
+    @board
+  end
+
+  def valid_move?(column)
+   true
+   #(0..6).include?(column)# && @board[column].length < 6
+  end
+
+  def add_piece(column)
+    @board.board[column.to_i] << 'X'
+  end
+
 end
 
 
-class Board
-  attr_accessor :board
-
-  def initialize(board=nil)
-    if board.nil?
-      @board = Array.new(7) { Array.new }
-    else
-      @board = board
-    end
-  end
-end
