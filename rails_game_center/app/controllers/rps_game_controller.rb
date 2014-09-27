0b89dca84d6bd20dc1f814067db27a46f9f85b74
Paul Hyman <mail@phyman.com>
2014-09-27 16:53:33 -0500

    Feature complete

--- rails_game_center/app/controllers/rps_game_controller.rb
+++ rails_game_center/app/controllers/rps_game_controller.rb
@@ -2,20 +2,18 @@ class RpsGameController < ApplicationController
 
   MOVES = ['rock', 'paper', 'scissors']
   def new
+    session[:your_score] ||= 0
+    session[:their_score] ||= 0
+    session[:player_move] ||= "none yet "
+    session[:ai_move] ||= "none yet "
+    session[:result] ||= " Please Play "
   end
 
   def create
-
-    session[:your_score] ||= 0
-    session[:their_score] ||= 0
-
     session[:player_move] = params[:choice]
     session[:ai_move] = MOVES.sample
-
-
     session[:result] = compare_fists(session[:player_move], session[:ai_move])
-
-
+    increment_score(session[:result])
     redirect_to new_rps_game_path
   end
 
@@ -47,11 +45,16 @@ class RpsGameController < ApplicationController
     end
   end
 
-  # def increment_score(result)
-  #   case result
-  #   when 'WIN!'
-  #   end
-  # end
+  def increment_score(result)
+    case result
+    when 'WIN!'
+      session[:your_score] +=1
+    when 'LOSE.'
+      session[:their_score] +=1
+    else
+      # it's a DRAW no one scores!
+    end
+  end
 
 end
 
