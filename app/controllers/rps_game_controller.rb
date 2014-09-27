b7b591c4017146cf11afbcb6ecb200ac9b35656a
pndpo <peterdepaulo@gmail.com>
2014-09-26 18:24:29 -0700

    it works

--- app/controllers/rps_game_controller.rb
+++ app/controllers/rps_game_controller.rb
@@ -1,37 +1,49 @@
 class RpsGameController < ApplicationController
 
-	
-	CHOICES = ["rock", "paper", "scissors"]
-
-	@victory = {:scissors => :rock, :rock => :paper, :paper => :scissors }
-
-	
+		CHOICES = ["rock", "paper", "scissors"]
 
 	def new
-		# @game = Game.new(params[])
 	end
 
 	def index
-		@game = Game.new
-		@player = session[:choice]
+		@draw = "DRAW"
+		@comp_win = "COMP WINS"
+		@player_win = "PLAYER WINS"
+		if((session[:comp_count])==nil)
+			session[:comp_count] = 0
+		end
+		if((session[:play_count])==nil) 
+			session[:play_count] = 0
+		end
+		if is_valid?(session[:choice])
+			@player = session[:choice].strip.downcase
+			@computer = session[:comp_choice].strip.downcase
+			@result = check_win(@player, @computer)
+		else
+			rps_game_index_path
+		end
 	end
 
 	def create
 		session[:choice] = params[:player_choice]
+		session[:comp_choice] = computer_chooses
 		redirect_to rps_game_index_path
 	end
 
 	def is_valid?(choice)
-		CHOICES.include?(choice)
+		!choice.nil?
 	end
 
 	def check_win(choice1, choice2)
+		@victory = {:scissors => :rock, :rock => :paper, :paper => :scissors }
 		if choice1 == choice2
-			puts "This is a draw"
-		elsif @victory[choice1] == choice2
-			puts "computer wins"
+			return @draw
+		elsif @victory[choice1.strip.to_sym] == choice2
+			session[:comp_count] += 1
+			return @comp_win
 		else
-			puts "player wins"
+			session[:play_count] += 1
+			return @player_win
 		end
 	end
 
@@ -44,11 +56,11 @@ end
 
 # to add feature to track play count
 
-# def conditions
-# 		if((session[:history])==nil)
-#   			session[:history] = Array.new
-# 		end
-# 		if((session[:counter])==nil)
-#   			session[:counter] = 0
-# 		end
-# 	end
+def conditions
+	if((session[:history])==nil)
+			session[:history] = Array.new
+	end
+	if((session[:counter])==nil)
+			session[:counter] = 0
+	end
+end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,12 +1,3 @@
 class Game 
 
-	
-
-	def get_choice
-		@player_choice = gets.strip.downcase # params[:player_choice]
-	end
-
-
-
-
 end
