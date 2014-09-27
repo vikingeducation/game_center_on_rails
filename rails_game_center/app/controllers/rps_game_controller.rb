117d79f02fc4343e872b4aea146f648febf483b1
Paul Hyman <mail@phyman.com>
2014-09-27 17:18:12 -0500

    Minor refactoring and added session clear on return to homepage

--- rails_game_center/app/controllers/rps_game_controller.rb
+++ rails_game_center/app/controllers/rps_game_controller.rb
@@ -1,49 +1,21 @@
 class RpsGameController < ApplicationController
 
-  MOVES = ['rock', 'paper', 'scissors']
-  def new
-    session[:your_score] ||= 0
-    session[:their_score] ||= 0
-    session[:player_move] ||= "none yet "
-    session[:ai_move] ||= "none yet "
-    session[:result] ||= " Please Play "
-  end
-
-  def create
-    session[:player_move] = params[:choice]
-    session[:ai_move] = MOVES.sample
-    session[:result] = compare_fists(session[:player_move], session[:ai_move])
-    increment_score(session[:result])
-    redirect_to new_rps_game_path
-  end
-
-  def compare_fists(player_1_choice, player_2_choice)
-    if player_1_choice == 'rock'
-      if player_2_choice == 'rock'
-        'DRAW.'
-      elsif player_2_choice == 'paper'
-        'LOSE.'
-      else
-        'WIN!'
-      end
-    elsif player_1_choice == 'paper'
-      if player_2_choice == 'rock'
-        'WIN!'
-      elsif player_2_choice == 'paper'
-        'DRAW.'
-      else
-        'LOSE.'
-      end
-    else
-      if player_2_choice == 'rock'
-        'LOSE.'
-      elsif player_2_choice == 'paper'
-        'WIN!'
-      else
-        'DRAW.'
-      end
+    MOVES = ['rock', 'paper', 'scissors']
+    def new
+      session[:your_score] ||= 0
+      session[:their_score] ||= 0
+      session[:player_move] ||= "none yet "
+      session[:ai_move] ||= "none yet "
+      session[:result] ||= " Please Play "
     end
-  end
+
+    def create
+      session[:player_move] = params[:choice]
+      session[:ai_move] = MOVES.sample
+      session[:result] = RpsGame.compare_fists(session[:player_move], session[:ai_move])
+      increment_score(session[:result])
+      redirect_to new_rps_game_path
+    end
 
   def increment_score(result)
     case result
@@ -52,9 +24,9 @@ class RpsGameController < ApplicationController
     when 'LOSE.'
       session[:their_score] +=1
     else
-      # it's a DRAW no one scores!
+      # it's a DRAW, no one scores!
     end
   end
 
-end
+end # /class
 
