40e0e9e4ba2fe3bca353399b728ee5b3a61a3a57
Tom McLaughlin <tim5046@gmail.com>
2014-09-28 10:43:43 -0400

    connect four fully functional but still ugly on screen

--- GameCenter/app/controllers/connect_four_controller.rb
+++ GameCenter/app/controllers/connect_four_controller.rb
@@ -1,15 +1,20 @@
 class ConnectFourController < ApplicationController
   def index 
+
     @game = Connectfour.new(session['current_board'])
     @board = @game.board.board
     session['current_board'] = @board
     session['game'] = @game
     @current_player = session['current_player']
+    @game_status = session['game_status']
+    flash[:status] = "Game over? #{@game_status}"
   end 
 
-  def select_create
+  def select_create #fired when we pick number of players
+    #this is a good place to reset all variables
     session['current_board'] = nil
     session['game'] = nil
+    session['game_status'] = false
     @current_player = 'X'
     save_player
     redirect_to connect_four_index_path
@@ -20,27 +25,42 @@ class ConnectFourController < ApplicationController
     if make_move(@current_player)
       swap_player
     end
+    check_game_over
+    redirect_to connect_four_index_path
   end
 
   private
 
   def make_move(current)
-    @game = session['game']
-    @update = @game.check_move(params[:c4choice],current) unless params[:c4choice].nil?
-    session['current_board'] = @update.board
-    redirect_to connect_four_index_path
+    unless params[:c4choice].nil?
+      @game = session['game']
+      if @game.check_move(params[:c4choice],current) 
+        @update = @game.add_piece(params[:c4choice],current) 
+        session['current_board'] = @update
+        session['game'] = @game
+      else
+        false
+      end
+    end
+
   end
 
 
   def swap_player
-    if session['current_player'] == 'X'
-      session['current_player'] = 'Y'
-    else
-      session['current_player'] = 'X'
-    end
+    session['current_player'] == 'X' ? session['current_player'] = 'Y' : session['current_player'] = 'X'
   end
+
   def save_player
     session['current_player'] = @current_player
+  end
+
+  def check_game_over
+    @game = session['game']
+    if @game.check_victory(params[:c4choice])
+      session['game_status'] = true
+      return true
+    end
+    false
   end
 
 
--- GameCenter/app/models/connectfour.rb
+++ GameCenter/app/models/connectfour.rb
@@ -12,14 +12,16 @@ class Connectfour
 
   def check_move(column,player)
     if valid_move?(column)
-      add_piece(column,player)
+      return true
+
+    else
+      return false
     end
-    @board
   end
 
   def valid_move?(column)
-   true
-   #(0..6).include?(column)# && @board[column].length < 6
+    column = column.to_i
+   (0..6).include?(column) && @board.board[column].length < 6
   end
 
   def add_piece(column,player)
@@ -28,6 +30,66 @@ class Connectfour
     else
       @board.board[column.to_i] << 'Y'
     end
+    @board.board
+  end
+
+
+
+
+  ################ game checks
+  def check_victory(last_move)
+    vertical_check(last_move) || horizontal_check(last_move) || diagonal_up_check(last_move) || diagonal_down_check(last_move)
+  end
+
+  def vertical_check(last_move)
+    last_move = last_move.to_i
+    result=true
+    0.upto(3) do |i|
+     result = false unless (@board.board[last_move][-1-i]  == @board.board[last_move][-1])
+    end
+    result
+  end
+
+  def horizontal_check(last_move)
+    last_move = last_move.to_i
+    result = true
+    color = @board.board[last_move][-1]
+    row = @board.board[last_move].length-1
+    same_count = 0
+    (0..6).each do |i|
+      if @board.board[i][row] == color
+        same_count += 1
+        return true if same_count == 4
+      else
+        same_count=0
+      end
+    end
+    false
+  end
+
+
+  def diagonal_up_check(last_move)
+    last_move = last_move.to_i
+    (0..6).each do |column|
+      (0..5).each do |row|
+        color = @board.board[column][row]
+        break unless @board.board[column+3] && @board.board[column+3][row+3]
+        return true if @board.board[column+1][row+1] == color && @board.board[column+2][row+2] == color && @board.board[column+3][row+3] == color
+      end
+    end
+    false
+  end
+
+  def diagonal_down_check(last_move)
+    last_move = last_move.to_i
+    (0..6).each do |column|
+      (0..5).each do |row|
+        color = @board.board[column][row]
+        break unless @board.board[column-3] && @board.board[column-3][row+3]
+        return true if @board.board[column-1][row+1] == color && @board.board[column-2][row+2] == color && @board.board[column-3][row+3] == color
+      end
+    end
+    false
   end
 
 end
