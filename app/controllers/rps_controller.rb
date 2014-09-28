5db8b81ab1a94127e214b8389072922b4c50e820
pndpo <peterdepaulo@gmail.com>
2014-09-28 16:02:54 -0700

    rock paper scissors better

--- app/controllers/rps_controller.rb
+++ app/controllers/rps_controller.rb
@@ -1,27 +1,26 @@
 class RpsController < ApplicationController
-
 	def new
 		@game = Game.new
-		session[:choice] = nil
-		session[:player_count] = 0
-		session[:comp_count] = 0
+		# session[:choice] = nil
+		# session[:player_count] = 0
+		# session[:comp_count] = 0
+		reset_session # does this replace all the above?
 	end
 
-	def index		
+	def index	#the results were being read as strings. Line 14 changes that.
 		@game = Game.new(session[:choice])
 		@comp_choice = @game.comp_choice
-		@winner = @game.winner
 		@player_choice = @game.player_choice
+		@winner = @game.game_win(@player_choice.to_i, @comp_choice.to_i) 
+		@choices = ["Rock", "Paper", "Scissors"]
 	end
 
 	def create
 		session[:choice] = params[:player_choice]
-
-		redirect_to rps_path
+		redirect_to rps_path #refactor to render a partial
 	end
 
 	def reset
-
 		redirect_to new_rp_path
 	end
 
