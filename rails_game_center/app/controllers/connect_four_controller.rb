9381fcd1e40b505c5d1377bd211fe0289b6fdd67
Paul Hyman <mail@phyman.com>
2014-09-28 14:48:51 -0500

    Mostly working with possible render offset issue

--- rails_game_center/app/controllers/connect_four_controller.rb
+++ rails_game_center/app/controllers/connect_four_controller.rb
@@ -2,6 +2,7 @@ class ConnectFourController < ApplicationController
 
   NUM_COLS = 7
   NUM_ROWS = 6
+  WIN_COUNT =4
 
   def new
     init_session_variables
@@ -10,7 +11,7 @@ class ConnectFourController < ApplicationController
   end
 
   def play_turn
-    
+
     #main board and submissions form.
     #like an EDIT
     load_session_variables
@@ -24,23 +25,35 @@ class ConnectFourController < ApplicationController
 
   def drop_piece
     #handles POSTS from move form
+    @col_selection = params[:column].to_i  # col num that was selected on the form
+    load_session_variables # to send to model
+    @connect_four_game_instance = ConnectFour.new(@board_arr, @curr_player)
 
-    #talks to controller, validates move
+    @connect_four_game_instance.accept_piece(@col_selection, @curr_player["play_piece"])
 
-    #redirects to play_turn if game ongoing
-    #redirects to show if game over
+   #redirects to show if game over
+    if check_game_over
+      redirect_to connect_four_show_path
+    else
+     #redirects to play_turn if game ongoing
+     redirect_to connect_four_play_turn_path
+    end
+
   end
 
   private
 
   def init_session_variables
-    @board_arr = Array.new(NUM_COLS) { Array.new(NUM_ROWS){ nil } }
+    @board_arr = Array.new(NUM_COLS) { Array.new(NUM_ROWS){ "_" } }
     session[:board] = @board_arr
 
     @player1 = {type: "human" , play_piece: "X"}
     session[:player1] = @player1
     @player2 = {type: "AI", play_piece: "O"}
     session[:player2] = @player2
+    @curr_player = @player1
+    session[:@curr_player] = @curr_player
+
   end
 
   def load_session_variables
@@ -48,8 +61,53 @@ class ConnectFourController < ApplicationController
     @num_rows = NUM_ROWS
 
     @board_arr = session[:board]
-
     @player1 = session[:player1]
     @player2 = session[:player2]
+    @curr_player = session[:@curr_player]
   end
+
+    def check_game_over
+      check_victory || check_draw
+    end
+
+    def check_victory
+      if winning_combination?
+        true
+      else
+        false
+      end
+    end
+
+    def check_draw
+      if @board_arr.all? do |col|
+          col.none?{|cell| cell=="_"}
+        end
+        # puts "The gameboard is full, it's a draw."
+        true
+      else
+        false
+      end
+    end
+
+    def winning_combination?
+      winning_diagonal?   ||
+      winning_horizontal? ||
+      winning_vertical?
+    end
+
+    def winning_diagonal?
+      # check if are four in a row on any verticals
+      return @connect_four_game_instance.diag_pieces_in_a_row?(WIN_COUNT, @curr_player["play_piece"])
+    end
+
+    def winning_vertical?
+      # check if are four in a row on any verticals
+      return @connect_four_game_instance.vert_pieces_in_a_row?(WIN_COUNT, @curr_player["play_piece"])
+    end
+
+    def winning_horizontal?
+      # check if are four in a row on any horizontals
+      return @connect_four_game_instance.horz_pieces_in_a_row?(WIN_COUNT, @curr_player["play_piece"])
+    end
+
 end
--- rails_game_center/app/models/connect_four.rb
+++ rails_game_center/app/models/connect_four.rb
@@ -1,2 +1,74 @@
 class ConnectFour #Plain Old Ruby Object
+
+  def initialize(board, curr_player)
+    @playfield = board
+    @current_player = curr_player
+  end
+
+    def accept_piece(col_num, piece)
+      @playfield[col_num - 1].each_with_index do |cell, index|
+        next unless cell == "_"
+        @playfield[col_num - 1][index] = piece
+        break
+      end
+    end
+
+    def diag_pieces_in_a_row?(piece_count, piece)
+      # check if there are {piece_count}
+      # number of pieces in a diagonal row
+      diagonals.any? do |diag|
+        if diag.size > 3
+          diag.join('').include?((piece * piece_count))
+        end
+      end
+    end
+
+    # game's X rows
+    def vert_pieces_in_a_row?(piece_count, piece)
+        # check if there are {piece_count}
+        # number of pieces in a vertical row
+        verticals.any? do |vert|
+            vert.join('').include?((piece * piece_count))
+          end
+    end
+
+    # game's Y columns
+    def horz_pieces_in_a_row?(piece_count, piece)
+        # check if there are {piece_count}
+        # number of pieces in a horizontal row
+        horizontals.any? do |horz|
+           horz.join('').include?((piece * piece_count))
+        end
+    end
+
+    def diagonals
+      [@playfield, @playfield.map(&:reverse)].inject([]) do |all_diags, matrix|
+      ((-matrix.count + 1)..matrix.first.count).each do |offet_index|
+        diagonal = []
+        (matrix.count).times do |row_index|
+          col_index = offet_index + row_index
+          diagonal << matrix[row_index][col_index] if col_index >= 0
+        end
+        all_diags << diagonal.compact if diagonal.compact.count > 1
+      end
+      all_diags
+    end
+    end
+
+    def verticals
+        # return the vertical pieces
+        @playfield
+    end
+
+    # horizontals
+    def horizontals
+        # return the horizontal pieces
+        horizontals = []
+        6.times do |i|
+            horizontals << [@playfield[0][i],@playfield[1][i],@playfield[2][i],@playfield[3][i],@playfield[4][i],@playfield[5][i],@playfield[6][i]]
+        end
+        horizontals
+    end
+
 end
+
