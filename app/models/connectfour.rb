08f0eebf88ef5f4c1a2240a40b3f633a02a89757
Andur Carr <carr.andur@gmail.com>
2014-10-02 00:29:55 -0700

    Refactor checks into one method

--- app/models/connectfour.rb
+++ app/models/connectfour.rb
@@ -1,51 +1,33 @@
 class Connectfour
   attr_accessor :board
 
-  def initialize(c4array=nil)
-    @board = c4array || Array.new(7){Array.new}
+  def initialize(c4array=nil, win_count=4, height=6, width=7)
+    @win_count, @height, @width = win_count, height, width
+    @board = c4array || Array.new(@width){Array.new}
+  end
+
+  def ceiling(step, dimension)
+    step > 0 ? dimension - @win_count : dimension - 1
+  end
+
+  def floor(step)
+    step < 0 ? @win_count - 1 : 0
   end
 
   def make_move(move, player)
     @board[move.to_i] << player
   end
 
-# Checks
-
   def check_victory
-    check_vertical || check_horizontal || check_diagonal_up || check_diagonal_down
+    check(0,1) || check(1,0) || check(1,1) || check(1,-1)
   end
 
-  def check_vertical
-    (0..6).each do |col|
-      (0..2).each do |row|
-        return true if @board[col][row] && (1..3).all? { |y| @board[col][row] == @board[col][row+y] }
-      end
-    end
-    false
-  end
-
-  def check_horizontal
-    (0..3).each do |col|
-      (0..5).each do |row|
-        return true if @board[col][row] && (1..3).all? { |x| @board[col][row] == @board[col+x][row]}
-      end
-    end
-    false
-  end
-
-  def check_diagonal_up
-    (0..3).each do |col|
-      (0..2).each do |row|
-        return true if @board[col][row] && (1..3).all? { |diag| @board[col][row] == @board[col+diag][row+diag]}
-      end
-    end
-    false
-  end
-
-  def check_diagonal_down
-    (0..3).each do |col|
-      (3..5).each do |row|
-        return true if @board[col][row] && (1..3).all? { |diag| @board[col][row] == @board[col+diag][row-diag]}
+  def check(h_step, v_step)
+    (floor(h_step)..ceiling(h_step, @width)).each do |col|
+      (floor(v_step)..ceiling(v_step, @height)).each do |row|
+        return true if @board[col][row] && (1..(@win_count-1)).all? do |x|
+          @board[col][row] == @board[col+x*h_step][row+x*v_step]
+        end
       end
     end
     false
