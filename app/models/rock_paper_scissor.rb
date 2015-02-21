c2abc6bc80e3b1c3e74906da3e7185dc9a4ae57e
Dan Berger <dsberger@gmail.com>
2015-02-21 13:02:52 -0800

    mostly working RPS game

--- app/models/rock_paper_scissor.rb
+++ app/models/rock_paper_scissor.rb
@@ -7,7 +7,17 @@ class RockPaperScissor
     @ai_move = [:rock, :paper, :scissors].sample
   end
 
-  def is_draw?
+  def player_won?
+    (player_move == :rock && ai_move == :scissors) ||
+    (player_move == :scissors && ai_move == :paper) ||
+    (player_move == :paper && ai_move == :rock)
+  end
+
+  def ai_won?
+    !(player_won? || draw?)
+  end
+
+  def draw?
     player_move == ai_move
   end
 end
