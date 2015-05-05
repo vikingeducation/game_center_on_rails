ad7cfbf45c048a1fb2aed4dac528fea9cf3f3fa1
niconan <nick.hax@gmail.com>
2015-05-05 10:26:38 -0700

    ugly but kind of works - breaks when AI makes move in full column

--- app/controllers/connectfour_controller.rb
+++ app/controllers/connectfour_controller.rb
@@ -1,6 +1,8 @@
 class ConnectfourController < ApplicationController
   def new
+    @current_player = 'Human'
     @game = ConnectFour.new.board
+    save_player
     save_board
 
     render :game_board
@@ -8,16 +10,51 @@ class ConnectfourController < ApplicationController
 
   def drop_piece
     @move = ConnectFour.new(get_board)
-    @move.make_move(get_column, 'R')
-    @move.tachikoma_move
-    @game = @move.board
-    save_board
+    if @move.column_full?(get_column)
+      @message = "Column full! Try again."
+      @game = @move.board
+
+      save_player
+      save_board
+    else
+      @move.make_move(get_column, 'R')
+      @current_player = get_player
+      if @move.player_won?
+        @game_over = "Game over, #{@current_player} wins!"
+      elsif @move.is_full?
+        @game_over = "Game over, it's a draw!"
+      end
+      switch_player
+      @current_player = get_player
+      @move.tachikoma_move
+      if @move.player_won?
+        @game_over = "Game over, #{@current_player} wins!"
+      elsif @move.is_full?
+        @game_over = "Game over, it's a draw!"
+      end
+      @game = @move.board
+
+      save_player
+      save_board
+    end
 
     render :game_board
   end
 
   def save_board
     session[:saved_board] = @game
+  end
+
+  def save_player
+    session[:current_player] = @current_player
+  end
+
+  def switch_player
+    @current_player = @current_player == 'Human' ? 'Tachikoma' : 'Human'
+  end
+
+  def get_player
+    session[:current_player]
   end
 
   def get_board
--- app/models/connect_four.rb
+++ app/models/connect_four.rb
@@ -21,4 +21,70 @@ class ConnectFour
     end
     @board[row][column] = color
   end
+
+  def is_full?
+    if @board.flatten.all?
+      true
+    end
+  end
+
+  def column_full?( column )
+    if @board[0][column] != nil
+      true
+    end
+  end
+
+  def player_won?
+    vertical? || horizontal? || up_diagonal? || down_diagonal?
+  end
+
+  def vertical?
+    @board.each do |row|
+      0.upto(2) do |col|
+        test_win = [ row[col], row[col+1], row[col+2], row[col+3] ]
+        return true if winning_combo?( test_win )
+      end
+    end
+    false
+  end
+
+  def horizontal?
+    col = 0
+    while col < 7 do
+      0.upto(3) do |row|
+        test_win = [ @board[row][col], @board[row+1][col], @board[row+2][col], @board[row+3][col] ]
+        return true if winning_combo?( test_win )
+      end
+      col += 1
+    end
+    false
+  end
+
+  def up_diagonal?
+    row = 0
+    while row < 4 do
+      0.upto(2) do |col|
+        test_win = [ @board[row][col], @board[row+1][col+1], @board[row+2][col+2], @board[row+3][col+3] ]
+        return true if winning_combo?( test_win )
+      end
+      row += 1
+    end
+    false
+  end
+
+  def down_diagonal?
+    row = 0
+    while row < 4 do
+      5.downto(3) do |col|
+        test_win = [ @board[row][col], @board[row+1][col-1], @board[row+2][col-2], @board[row+3][col-3] ]
+        return true if winning_combo?( test_win )
+      end
+      row += 1
+    end
+    false
+  end
+
+  def winning_combo?( test_win )
+    test_win.all? && test_win.uniq.length == 1
+  end
 end
