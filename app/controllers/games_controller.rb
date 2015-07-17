06b2e08ccf6045951d188450051288964b4da815
alokp <alokpra@gmail.com>
2015-07-17 15:25:47 -0700

    rendering game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,25 +1,23 @@
 class GamesController < ApplicationController
   include GamesHelper
   def index
-    if session["game_over"] == true
-      @game_over = true
-      session["game_over"] = false
-      session["board"] = new_board
+    if params["restart"]
+    	session["board"] = new_board
+    	session["game_over"] = false
     end
+    @game_over = session["game_over"]
     session["board"] ||= new_board
     @board = session["board"]
   end
 
   def move
     board = session["board"]
-
     flash[:notice] = "Illegal move, try again." unless play_move(params[:col].to_i, board)
-
     if check_winner(board)
       flash[:notice] = "Player #{check_winner(board) + 1} wins!"
       session["game_over"] = true
     end
-    redirect_to index_path
+    	redirect_to index_path
   end
 
 end
