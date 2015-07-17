c8f71946ab1fcefaf1b2dbe9c414663fb139806f
shadefinale <shadefinale@gmail.com>
2015-07-17 13:57:24 -0500

    Planning out connect four.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,20 @@
 class GamesController < ApplicationController
+  include GamesHelper
+  def index
+    session["board"] = new_board unless session["board"]
+  end
+
+  def move
+    board = session["board"]
+    if board
+      flash[:notice] = "Illegal move, try again." unless play_move(params[:move], board)
+      check_winner
+    else
+      board = new_board
+    end
+
+    redirect_to index_path
+  end
 
 
 end
--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,17 @@
 module GamesHelper
+  def get_or_create_board
+    board = session["state"] if session["state"]
+    board ||= new_board
+    return board
+  end
+
+  def new_board
+    return [[],[],[],[],[],[],[]]
+  end
+
+  def play_move(move, board)
+    return false if board[move].length >= 6
+    turn = board.flatten.length % 2
+    board[move] << turn
+  end
 end
