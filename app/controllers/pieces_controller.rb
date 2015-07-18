b0c3271dc8183174bb2a92bf77725e25393f5324
ayva <olga.ayva@gmail.com>
2015-07-17 17:18:27 -0700

    won conditions

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -22,7 +22,6 @@ class PiecesController < ApplicationController
   end
 
   def move
-
     p = Piece.find_by(x_value: params[:move], y_value: find_y)
     if session[:player]=="Olga"
       p.color = 1
@@ -30,6 +29,7 @@ class PiecesController < ApplicationController
       p.color = 2
     end
     p.save
+    flash.notice = "#{session[:player]} won!" if vertical_win?(p.color) || horizontal_win?(p.color)||diagonal_win?(p.color)
     player_name_switch
     redirect_to pieces_path
   end
@@ -67,7 +67,43 @@ class PiecesController < ApplicationController
         @piece.save
       end
     end
-
   end
+
+  def vertical_win?(symbol)
+
+    0.upto(6) do |col|
+      0.upto(2) do |row|
+        if [@field[row][col],@field[row+1][col],@field[row+2][col],@field[row+3][col]].all?  {|place| place == symbol}
+
+          return true
+        end
+      end
+    end
+
+    false
+  end
+
+
+  def horizontal_win?(symbol)
+    @field.each do |row|
+      0.upto(3) do |index|
+        if row[index..index+3].all? {|place| place == symbol}
+          return true
+        end
+      end
+    end
+    false
+  end
+
+  def diagonal_win?(symbol) #not all checks compatible
+    #top right= row 0, col 0; bottom left= row 5, col6
+    0.upto(3) do |col|
+      5.downto(3) do |row|
+        break if !(0..5).include?(row+3) || !(0..6).include?(col-3)
+        if [@field[row][col],@field[row+1][col-1],@field[row+2][col-2],@field[row+3][col-3]].all?  {|place| place == symbol}
+          return true
+        end
+      end
+    end
 
 end
