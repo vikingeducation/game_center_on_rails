2b5717ffc1e33f949257e3a4cf6d59302d603176
shadefinale <shadefinale@gmail.com>
2015-07-17 19:38:40 -0500

    We made it work

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,10 +7,10 @@ class GamesController < ApplicationController
     @board = session["board"]
   end
 
-# This is the first page a user see
+  # This is the first page a user see
   def new
     if params["player_count"]
-      if params["player_count"] == 1
+      if params["player_count"].to_i == 1
         session["players"] = 1
       else
         session["players"] = 2
@@ -23,22 +23,9 @@ class GamesController < ApplicationController
 
   def move
     board = session["board"]
-    unless play_move(params[:col].to_i, board)
-    	flash[:notice] = "Illegal move, try again."
-    else
-    	if check_winner(board)
-      		flash[:notice] = "Player #{check_winner(board) + 1} wins!"
-      		session["game_over"] = true
-      	else
-      		ai_move = AI.new(board).move
-      		play_move(ai_move, board)
-      		if check_winner(board)
-      			flash[:notice] = "Player #{check_winner(board) + 1} wins!"
-      			session["game_over"] = true
-      		end
-      	end
-    end
-    	redirect_to index_path
+    play(params[:col].to_i, board)
+
+    redirect_to index_path
   end
 
 end
