0fbb1c55717e7c3e6b163480c7e6efae5c626586
Jeffrey Gisin <jgisin@gmail.com>
2016-01-25 16:25:03 -0700

    Stole old game

--- Connect4/app/controllers/connect_four_controller.rb
+++ Connect4/app/controllers/connect_four_controller.rb
@@ -1,12 +1,18 @@
 class ConnectFourController < ApplicationController
 
   def new
-    game = ConnectFour.new
-    @board = game.board
-    
+    if session[:board]
+      board = JSON.parse(session[:board])
+      game = ConnectFour(board)
+    else
+      game = ConnectFour.new
+      board = game.board.board
+    end
+    session[:board] = game.board.board.to_json
+    @board = board 
   end
 
   def drop_piece
-     
+     #session[:game] = ConnectFou
   end  
 end
