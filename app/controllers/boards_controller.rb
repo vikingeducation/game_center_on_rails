530087ffe686f59a3c3bc774c57057060fe4def3
Andrew Baik <and.baik@gmail.com>
2016-01-25 19:49:21 -0500

    finish board render and move routes with error message

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,19 +1,26 @@
 class BoardsController < ApplicationController
 
   def index
-    @board = Board.new(session[:board]).board
-    @player = true
+
+    @board = Board.new.board
+    session[:board] = @board
+    session[:player] = true
+    @player = session[:player]
   end
 
   def move
     @board_object = Board.new(session[:board])
     if @board_object.valid_move?(params[:column])
-      @board_object.place_disk(@player, params[:column])
-      @player = !@player
+      @board_object.place_disk(session[:player], params[:column])
+      session[:player] = !session[:player]
+      @player = session[:player]
       @board = @board_object.board
       session[:board] = @board
     else
-
+      flash.now[:error] = "Invalid move!"
+      @board = session[:board]
+      @player = session[:player]
+      render :move
     end
   end
 end
--- app/models/board.rb
+++ app/models/board.rb
@@ -1,20 +1,20 @@
 class Board
 
   attr_accessor :board
-  
-  def initialize(board)
+
+  def initialize(board=nil)
     @board = board ? board : Array.new(6) { Array.new(7, 0) }
   end
 
   def valid_move?(player_input)
-    @board[6 - 1][player_input.to_i - 1].nil? ? true : false
+    @board[6 - 1][player_input.to_i - 1] == 0 ? true : false
   end
 
   #add disk object to appropraite column
-  def place_disk ( disk, column )
-    ( 0...6 ).each do | row |
-      if !@board[row][column]
-        @board[row][column] = disk ? 1 : 2
+  def place_disk ( player_boolean, column )
+    (0...6).each do |row|
+      if @board[row][column.to_i - 1] == 0
+        @board[row][column.to_i - 1] = player_boolean ? 1 : 2
         break
       end
     end
