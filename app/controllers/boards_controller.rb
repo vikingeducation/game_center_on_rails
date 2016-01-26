9ca23b732b10e9f0f56a2cf1e6c984bcb18e15e6
Andrew Baik <and.baik@gmail.com>
2016-01-25 20:11:37 -0500

    finished game

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,26 +1,34 @@
 class BoardsController < ApplicationController
 
   def index
-
     @board = Board.new.board
     session[:board] = @board
     session[:player] = true
     @player = session[:player]
+    session[:victory] = false
   end
 
   def move
     @board_object = Board.new(session[:board])
     if @board_object.valid_move?(params[:column])
       @board_object.place_disk(session[:player], params[:column])
-      session[:player] = !session[:player]
+
+      session[:victory] = @board_object.win_conditions?
+      @victory = session[:victory]
+
+      session[:player] = !session[:player] unless @victory
       @player = session[:player]
       @board = @board_object.board
       session[:board] = @board
     else
-      flash.now[:error] = "Invalid move!"
+      flash.now[:error] = @board_object.grid_full? ? "Grid is full!" : "Invalid move!"
       @board = session[:board]
       @player = session[:player]
       render :move
     end
   end
+
+  private
+
+
 end
--- app/models/board.rb
+++ app/models/board.rb
@@ -2,6 +2,10 @@ class Board
 
   attr_accessor :board
 
+  NUM_ROWS = 6
+  NUM_COLS = 7
+  FOUR = 4
+
   def initialize(board=nil)
     @board = board ? board : Array.new(6) { Array.new(7, 0) }
   end
@@ -19,4 +23,80 @@ class Board
       end
     end
   end
+
+  #return true if connect 4 exists and game ends
+  def win_conditions?
+    check_rows || check_cols || check_diagonals
+  end
+
+  #check for connect 4 along rows
+  def check_rows
+    @board.each do |row|
+      (0...NUM_COLS - FOUR + 1).each do |i|
+        return check_connect_four?( row[i...i+FOUR] )
+      end
+    end
+    false
+  end
+
+  #check every column for 4 vertical
+  def check_cols
+    (0...NUM_COLS).each do |col|
+      (0...NUM_ROWS - FOUR + 1).each do |row|
+        col_array = []
+        (0...FOUR).each do |index|
+          col_array << @board[row + index][col]
+        end
+        return true if check_connect_four?(col_array)
+      end
+    end
+    false
+  end
+
+  #check diagonals for connect 4
+  def check_diagonals
+    #diagonals from top left to bottom right
+    (0..NUM_ROWS - FOUR).each do | row |
+      (0..NUM_COLS - FOUR).each do |  col |
+        diag_array = []
+        (0...FOUR).each do |  square |
+          diag_array << @board[row + square][col + square]
+        end
+        return true if check_connect_four?(diag_array)
+      end
+    end
+    #diagonals from top right to bottom left
+   (0..NUM_ROWS - FOUR).each do | row |
+      (NUM_COLS - 1).downto ( NUM_COLS - FOUR ) do |  col |
+        diag_array = []
+        (0...FOUR).each do |  square |
+          diag_array << @board[row + square][col - square]
+        end
+        return true if check_connect_four?(diag_array)
+      end
+    end
+    false
+  end
+
+  #checks array for connect 4
+  def check_connect_four?(arr)
+    #checks for full array
+    return false if arr.any?(&:nil?)
+    #if array is full then check for winner
+    return true if arr.all? { |square| square == 1 }
+    return true if arr.all? { |square| square == 2 }
+    #otherwise return false
+    false
+  end
+
+  #return true if grid is full, else return false on empty square
+  def grid_full?
+    @board.each do |row|
+      row.each do |square|
+        return false if square == nil
+      end
+    end
+    true
+  end
+
 end
