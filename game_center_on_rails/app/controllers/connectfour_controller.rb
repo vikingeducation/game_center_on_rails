479d4c1f8c27e04b50f755c30389d3df7f51a4a3
Adrian Mui <adrian.mui@gmail.com>
2016-07-26 18:21:58 -0400

    m

--- game_center_on_rails/app/controllers/connectfour_controller.rb
+++ game_center_on_rails/app/controllers/connectfour_controller.rb
@@ -2,15 +2,22 @@
 
 class ConnectfourController < ApplicationController
 
-  enable :sessions 
-  
   def index
     @board = Board.new   
-
+    session['game_board'] = @board.game_board
   end
 
-  def 
+  def create
+    @board = Board.new
+    @board.game_board = session['game_board'] 
+
+    move = params[:col]
     
+    @board.add_piece(move, :x)
+
+    session['game_board'] = @board.game_board
+
+    redirect_to root_path
   end
 
 end
