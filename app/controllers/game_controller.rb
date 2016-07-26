a59a3141c63ccd56e86b67a3cf53d8452dfefead
cjvirtucio87 <cjv287@gmail.com>
2016-07-26 14:33:15 -0700

    Added rendering on view.

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -8,18 +8,18 @@ class GameController < ApplicationController
 
   def play
     move = params[:move].to_i
-    color = session[:color]
-    board = Board.new(session['board'])
-    if board.valid_move?(move, color)
-      board.add_piece(move, color)
-      flash[:success] = "Your peice has been added!"
+    @color = session[:color]
+    @board = Board.new(session['board'])
+    if @board.valid_move?(move, @color)
+      @board.add_piece(move, @color)
+      flash[:success] = "Your piece has been added!"
       session['color'] = session['color'] == "R" ? "B" : "R"
-      session['board'] = board.play_field
+      session['board'] = @board.play_field
     else
       flash[:danger] = "That move is invalid... please try again"
     end
     #if board has victory
-    if board.has_victory? || board.is_draw?
+    if @board.has_victory? || @board.is_draw?
       render :end
     else
       redirect_to :home
@@ -27,3 +27,9 @@ class GameController < ApplicationController
   end
 
 end
+
+# (1) Make end render dynamically.
+# (2) Validate input.
+# (3) Full-column should be invalid input.
+# (4) Make reset button on end page.
+# (5) Make flash messages appear.
