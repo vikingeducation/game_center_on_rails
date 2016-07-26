0ddd63e93ea4e3054543a665d54c2216081e02c4
Graham Turner <turnertgraham@gmail.com>
2016-07-26 14:28:30 -0400

    added logic to controller

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,11 +1,25 @@
+require_relative "../models/classes/board"
+
 class GameController < ApplicationController
-  
+
   def home
-    session['board'] ||= Board.new
-    
+    session['color'] ||= "red"
+    @board = session['board'] || Board.new.play_field
+    session['board'] = @board
   end
 
   def play
+    move = params[:move].to_i
+    color = session[:color]
+    board = board.new(session['board'])
+    if board.valid_move(move, color)
+      board.add_piece(move, color)
+      flash[:success] = "Your peice has been added!"
+      session['color'] = session['color'] == "red" ? "black" : "red"
+    else
+      flash[:danger] = "That move is invalid... please try again"
+    end
+    redirect_to :home
   end
 
 end
