ec8cc3c1004646221387de28787f6c5c1c9b6280
cjvirtucio87 <cjv287@gmail.com>
2016-07-26 11:02:38 -0700

    Added classes

--- app/models/classes/game.rb
+++ app/models/classes/game.rb
@@ -1,2 +1,60 @@
+#from VCS solution
 class Game
+  def initialize(game_type=nil)
+    @board = Board.new
+    @player1 = Player.new(@board, "\u{2686}")
+    game_type ||= choose_game_type
+    game_type == 1 ? @player2 = ConnectFourAI.new(@board, "\u{2688}") : @player2 = Player.new(@board, "\u{2688}")
+  end
+
+
+  def choose_game_type
+    puts "Welcome to Connect Four!"
+    puts ""
+    choice = nil
+
+    loop do
+      puts "#################################################"
+      puts "Select the game number that you'd like to play:"
+      puts "1)Single player VS. AI\n2)Two player"
+      puts "#################################################"
+
+      choice = gets.chomp.to_i  
+      break if [1, 2].include?(choice)
+    end
+
+    choice
+  end
+
+  def play
+    player = @player1
+
+    loop do
+      @board.render
+      player.move
+      player = swap_players(player)
+
+      break if connect_four? || draw?
+    end
+
+    game_over(player)
+  end
+
+  def swap_players(player)
+    player == @player1 ? @player2 : @player1
+  end
+
+  def connect_four?
+    @board.has_victory?
+  end
+
+  def draw?
+    @board.is_draw?
+  end
+
+  def game_over(player)
+    @board.render
+    puts "Ay, yo, Game Over"
+    puts player == @player1 ? "Player 2 wins!" : "Player 1 wins!"
+  end
 end
--- app/models/classes/player.rb
+++ app/models/classes/player.rb
@@ -1,2 +1,27 @@
+#from VCS solution
 class Player
+
+  # component of Game class, so you don't see it
+  # needs to know about the board so it can send messages there
+  # needs to know its own color, but not anybody else's
+  def initialize(board, color)
+    @board = board
+    @color = color
+  end
+
+  def move
+    column = nil
+    loop do
+      column = get_move
+      break if @board.valid_move?(column, @color)
+    end
+    @board.add_piece(column, @color)
+  end
+
+  private
+  def get_move
+    puts "Enter a column to play your piece (0-6): "
+    move = gets.chomp.to_i
+  end
+
 end
