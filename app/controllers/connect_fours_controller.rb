b02ca5a38a6292b0f3c293360339cea9ba17a27b
David Jiang <davidjgrey@gmail>
2016-07-26 14:46:18 -0700

    working on board methods

--- app/controllers/connect_fours_controller.rb
+++ app/controllers/connect_fours_controller.rb
@@ -1,5 +1,14 @@
 class ConnectFoursController < ApplicationController
 
   def new
+    @board = ConnectFour.new
+    session[:board] = @board.active_board
+  end
+
+  def create
+    @board = ConnectFour.new(session[:board])
+    column = params[:column]
+    @board.add_piece(column)
+    session[:board]= @board.active_board
   end
 end
--- app/models/connect_four.rb
+++ app/models/connect_four.rb
@@ -1,2 +1,28 @@
 class ConnectFour < ActiveRecord::Base
+
+  def initialize(new_board=board)
+    @active_board = new_board
+  end
+
+  def add_piece(column)
+
+  end
+
+  def find_available_row(column)
+    (0..5).each do |i|
+      if @active_board[[i,column]]==nil
+        return i
+      end
+    end
+  end
+
+  def board
+    board = Hash.new
+    ("0".."5").each do |i|
+      ("0".."6").each do |j|
+        board[[i,j]]=nil
+      end
+    end
+    board
+  end
 end
