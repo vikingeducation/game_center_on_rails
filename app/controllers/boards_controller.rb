42ee432c0863329e59120fb6e02c61a8d191f893
Jonathan Popenuck <john.popenuck@gmail.com>
2016-11-22 15:03:24 -0800

    DRYed up code

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,23 +1,19 @@
 class BoardsController < ApplicationController
-
+  include BoardsHelper
+  
   def show
-    session[:player] = "X" if session[:player].nil?
-    @board = Board.new(session[:layout]) || Board.new
+    reset
+    setup
+    @game_over = params[:game_over]
   end
 
   def update
     @board = Board.new(session[:layout])
-    unless last_move = @board.add_piece(params[:column].to_i, session[:player])
+    unless add_piece
       flash[:danger] = "Invalid move."
-      redirect_to root_path
     else
-      session[:layout] = @board.layout
-      if @board.four_connected?(last_move)
-        redirect_to game_over_path
-      else
-        session[:player] == "X" ? session[:player] = "O" : session[:player] = "X"
-        redirect_to root_path
-      end
+      make_move
     end
   end
+
 end
--- app/helpers/boards_helper.rb
+++ app/helpers/boards_helper.rb
@@ -1,2 +1,42 @@
 module BoardsHelper
+
+  def reset
+    if params[:reset]
+      session[:player] = "X"
+      session[:layout] = nil
+    end
+  end
+
+  def setup
+    session[:player] = "X" if session[:player].nil?
+    @board = Board.new(session[:layout]) || Board.new
+  end
+
+  def switch_player
+    session[:player] == "X" ? session[:player] = "O" : session[:player] = "X"
+  end
+
+  def end_game
+    flash[:success] = "#{session[:player]} wins!"
+    redirect_to root_path(:game_over => true)
+  end
+
+  def add_piece
+    @last_piece = @board.add_piece(params[:column].to_i, session[:player])
+  end
+
+  def game_over?
+    @board.four_connected?(@last_piece)
+  end
+
+  def make_move
+    session[:layout] = @board.layout
+    if game_over?
+      end_game
+    else
+      switch_player
+      redirect_to root_path
+    end
+  end
+
 end
