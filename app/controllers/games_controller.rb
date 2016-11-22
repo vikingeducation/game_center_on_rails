90552c4162b7a35c868658c21ec121603e0dc320
James Harris <computersherpa@gmail.com>
2016-11-22 17:22:03 -0600

    Create new game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,6 +8,10 @@ class GamesController < ApplicationController
   def new
   end
 
+  def show
+
+  end
+
   def create
     user_color = "##{params[:game][:color]}"
     unless user_color.empty?
@@ -15,6 +19,9 @@ class GamesController < ApplicationController
     else
       session[:color] = "#154360"
     end
-
+    @board = Gameboard::Board.new(width: 7, height: 6).to_yaml
+    @game = SaveGame.create(board: @board)
+    session[:game] = @game.id
+    redirect_to(game_path(@game.id))
   end
 end
