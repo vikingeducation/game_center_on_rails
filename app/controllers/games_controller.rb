e0eb3436c0f8f6faffd591083c36388f83caea85
James Harris <computersherpa@gmail.com>
2016-11-22 17:47:04 -0600

    Display board

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,7 +9,8 @@ class GamesController < ApplicationController
   end
 
   def show
-
+    # raise
+    @game = YAML.load(SaveGame.find_by_id(session[:game]).board)
   end
 
   def create
@@ -19,7 +20,13 @@ class GamesController < ApplicationController
     else
       session[:color] = "#154360"
     end
-    @board = Gameboard::Board.new(width: 7, height: 6).to_yaml
+    # @board = Gameboard::Board.new(width: 7, height: 6).to_yaml
+    example_arr = [
+      ["1", "2", "3"], #=> coordinates [[0,2], [1,2], [2,2]]
+      ["A", "B", "C"], #=> coordinates [[0,1], [1,1], [2,1]]
+      ["X", "Y", "Z"] #=> coordinates [[0,0], [1,0], [2,0]]
+    ]
+    @board = Gameboard::Board.new(preset: example_arr).to_yaml
     @game = SaveGame.create(board: @board)
     session[:game] = @game.id
     redirect_to(game_path(@game.id))
