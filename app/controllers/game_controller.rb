ad11dc59f61aedb2a1a955a2bb022ee4c1949926
Jessica G <gillan.jessica@gmail.com>
2016-11-22 16:29:16 -0700

    start skeleton

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,24 @@
+class GameController < ApplicationController
+  def new
+    session[:board] = 
+  end
+
+  def destroy
+  end
+
+  def edit
+
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def create
+  end
+end
