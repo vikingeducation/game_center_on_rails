02e0a8c5b1fdc33f8845d803e56ece3ad0f86fb9
Jonathan Popenuck <john.popenuck@gmail.com>
2016-11-22 11:47:30 -0800

    rendering successful

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,13 +1,17 @@
 class BoardsController < ApplicationController
 
   def show
+    session[:player] = "X" if session[:player].nil?
     @board = Board.new(session[:layout]) || Board.new
   end
 
   def update
     @board = Board.new(session[:layout])
-    @board.add_piece(params[:column], params[:piece])
+    @board.add_piece(params[:column].to_i, session[:player])
     session[:layout] = @board.layout
+    session[:player] == "X" ? session[:player] = "O" : session[:player] = "X"
     redirect_to root_path
   end
+
+
 end
