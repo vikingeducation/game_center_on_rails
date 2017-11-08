f32ba1919363988cd411adf881d9e39ec90bc2d6
anne <richardson.ae@gmail.com>
2017-11-08 11:47:50 -0600

    Polish headers for heroku deploy

--- app/controllers/blackjack_controller.rb
+++ app/controllers/blackjack_controller.rb
@@ -13,14 +13,14 @@ class BlackjackController < ApplicationController
     @player.set_hand_value
 
     # save objects to session
-    session['deck'] = deck
-binding.pry
-    session['dealer_hand'] = @dealer.hand
-    session['dealer_hand_value'] = @dealer.hand_value
+    # session['deck'] = deck
 
-    session['player_hand'] = @player.hand
-    session['player_hand_value'] = @player.hand_value
-    session['player_bankroll'] = @player.bankroll
+    # session['dealer_hand'] = @dealer.hand
+    # session['dealer_hand_value'] = @dealer.hand_value
+
+    # session['player_hand'] = @player.hand
+    # session['player_hand_value'] = @player.hand_value
+    # session['player_bankroll'] = @player.bankroll
 
     # output objects to view
     # render :blackjack, locals: { dealer: dealer, player: player, bet: 0, game_over: game_over }
